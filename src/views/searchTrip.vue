208679edfaaf4c45459a6a847ebc9529335127cd
mayunited12 <mayunited12@yahoo.com>
2021-06-01 12:49:25 +0100

    intro. search page

---
+++ src/views/searchTrip.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { query: {}, searchResult: {} };
+  },
+  created() {
+    this.query = this.$route.query;
+  },
+};
+</script>
+
+<style></style>
