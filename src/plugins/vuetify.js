1299429385d46b185d040e48a9d4d26038501a80
mayunited12 <mayunited12@yahoo.com>
2021-06-02 12:42:29 +0100

    import material design icons css

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,4 @@
-import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
